5cbe42fc18b6b25faf4d9c5e2e1e750d56653ec2
Roman Kovtun <9696465@gmail.com>
2024-02-10 23:00:34 +0300

    Project Bubbles Water class refactoring

--- src/project/liquids/Water.java
+++ src/project/liquids/Water.java
@@ -1,10 +1,17 @@
 package project.liquids;
 
 public abstract class Water {
-    private String color;
-    private String transparency;
-    private String smell ;
-    private String temperature;
+    private String color = "no";
+    private String transparency = "transparent";
+    private String smell = "no";
+    private int temperature = 0;
+
+    public Water(String color, String transparency, String smell, int temperature) {
+        this.color = color;
+        this.transparency = transparency;
+        this.smell = smell;
+        this.temperature = temperature;
+    }
 
     public String getColor() {
         return color;
@@ -30,11 +37,11 @@ public abstract class Water {
         this.smell = smell;
     }
 
-    public String getTemperature() {
+    public int getTemperature() {
         return temperature;
     }
 
-    public void setTemperature(String temperature) {
+    public void setTemperature(int temperature) {
         this.temperature = temperature;
     }
 }
