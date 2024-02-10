3ce0feaead606b2a956690b9141b205254adc30c
Roman Kovtun <9696465@gmail.com>
2024-02-10 18:09:11 +0300

    Main.java deleted

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello World");
-
-    }
-}
