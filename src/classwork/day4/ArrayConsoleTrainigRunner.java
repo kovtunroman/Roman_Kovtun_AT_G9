9dbb552d376626be78cbcc53a7c5dd2c6d26e542
Roman Kovtun <9696465@gmail.com>
2024-02-10 21:57:25 +0300

    ArrayConsoleTraining 1.2 subtask: Reversed Array with elements of inputArray that bigger then Comparable value

--- src/classwork/day4/ArrayConsoleTrainigRunner.java
+++ src/classwork/day4/ArrayConsoleTrainigRunner.java
@@ -1,9 +1,14 @@
 package classwork.day4;
 
+import java.util.Arrays;
+
 public class ArrayConsoleTrainigRunner {
     public static void main(String[] args) {
         int[] demoArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int demoIntDelimiter = 5;
-        System.out.println(ArrayConsoleTraining.sumOfEachNElementInArray(demoArray, demoIntDelimiter));
+        System.out.println("ArrayConsoleTraining 1.1: " + ArrayConsoleTraining.sumOfEachNElementInArray(demoArray, demoIntDelimiter));
+        System.out.println("ArrayConsoleTraining 1.2: ");
+        System.out.println(Arrays.toString(ArrayConsoleTraining.reverseArrayWithElementsBiggerThenInThisArray(demoArray, demoIntDelimiter)));
+
     }
 }
--- src/classwork/day4/ArrayConsoleTraining.java
+++ src/classwork/day4/ArrayConsoleTraining.java
@@ -14,4 +14,47 @@ public class ArrayConsoleTraining {
         }
         return sum;
     }
+
+    /*
+     * Метод, который вернет целочисленный массив, состоящий только из тех элементов исходного
+     * целочисленного массива (подается на вход этого метода), которые больше, чем число n,
+     * где n - целое число, передаваемое в сигнатуру этого метода, элементы в результирующем
+     * массиве должны быть расположены в обратном порядке
+     */
+    public static int[] reverseArrayWithElementsBiggerThenInThisArray(int[] inputIntArray, int intComparable) {
+
+        int[] resultArray = arrayWithElementsBiggerThenInThisArray(inputIntArray, intComparable);
+        return reverseArray(resultArray);
+
+    }
+
+    private static int[] arrayWithElementsBiggerThenInThisArray(int[] inputIntArray, int intComparable) {
+
+        int[] resultArray = new int[numberOfArrayElemetsBiggerThenComparable(inputIntArray, intComparable)];
+        int resultArrayElemntsIndex = 0;
+        for (int i = 0; i < inputIntArray.length; i++) {
+            if (inputIntArray[i] > intComparable) {
+                resultArray[resultArrayElemntsIndex++] = inputIntArray[i];
+            }
+        }
+        return resultArray;
+    }
+
+    private static int numberOfArrayElemetsBiggerThenComparable(int[] inputIntArray, int intComparable) {
+        int numberOfElementsBiggerThenComparable = 0;
+        for (int i = 0; i < inputIntArray.length; i++) {
+            if (inputIntArray[i] > intComparable) {
+                numberOfElementsBiggerThenComparable++;
+            }
+        }
+        return numberOfElementsBiggerThenComparable;
+    }
+
+    private static int[] reverseArray(int[] inputArray) {
+        int[] resultArray = new int[inputArray.length];
+        for (int i = 0; i < inputArray.length; i++) {
+            resultArray[resultArray.length - i - 1] = inputArray[i];
+        }
+        return resultArray;
+    }
 }
