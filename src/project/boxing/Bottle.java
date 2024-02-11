fe99e00520025d97e110c7f3746052a78c2080d2
Roman Kovtun <9696465@gmail.com>
2024-02-11 11:34:15 +0300

    Project Bubbles casting changes

--- src/project/boxing/Bottle.java
+++ src/project/boxing/Bottle.java
@@ -2,34 +2,31 @@ package project.boxing;
 
 import project.liquids.Bubble;
 import project.liquids.SparklingWater;
+import project.liquids.Water;
 
 public class Bottle {
     private double volume;
-    private SparklingWater water;
+    private Water water;
 
     public Bottle(double volume) {
-        this(volume, new SparklingWater());
-    }
-
-    public Bottle(double volume, SparklingWater water) {
         this.volume = volume;
-        this.water = water;
-        setBubbles();
     }
 
-    public SparklingWater getWater() {
+    public Water getWater() {
         System.out.printf("Water in the bottle: %s", water).println();
         return water;
     }
 
-    public void setWater(SparklingWater water) {
+    public void setWater(Water water) {
         System.out.printf("Water in the bottle set to: %s", water).println();
         this.water = water;
     }
 
     public void open() {
         System.out.println("Bottle was opened");
-        this.water.setOpened();
+        if (water instanceof SparklingWater) {
+            ((SparklingWater) this.water).setOpened();
+        }
     }
 
     public void setBubbles() {
@@ -37,7 +34,9 @@ public class Bottle {
         for (int i = 0; i < bubbles.length; i++) {
             bubbles[i] = new Bubble("CO2");
         }
-        water.pump(bubbles);
+        if (water instanceof SparklingWater) {
+            ((SparklingWater) this.water).pump(bubbles);
+        }
         System.out.printf("Bottle water bubbles were set. Number of bubbles: %s", bubbles.length).println();
     }
 
