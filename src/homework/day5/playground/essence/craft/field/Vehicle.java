8aa022bdee89f2b78238ab8cce8597567e64c10e
Roman Kovtun <9696465@gmail.com>
2024-02-14 00:05:28 +0300

    Homework Day5 Task2: Abstract classes refactoring constructors and geters and setters added

--- src/homework/day5/playground/essence/craft/field/Vehicle.java
+++ src/homework/day5/playground/essence/craft/field/Vehicle.java
@@ -3,4 +3,18 @@ package homework.day5.playground.essence.craft.field;
 import homework.day5.playground.essence.Matter;
 
 public abstract class Vehicle extends Matter {
+    protected String name;
+
+    public Vehicle(String name, int weight, int mass) {
+        super(weight, mass);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/homework/day5/playground/essence/creatures/Vegetable.java
+++ src/homework/day5/playground/essence/creatures/Vegetable.java
@@ -3,4 +3,18 @@ package homework.day5.playground.essence.creatures;
 import homework.day5.playground.essence.Matter;
 
 public abstract class Vegetable extends Matter {
+    protected String name;
+
+    public Vegetable(String name, int weight, int mass) {
+        super(weight, mass);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
