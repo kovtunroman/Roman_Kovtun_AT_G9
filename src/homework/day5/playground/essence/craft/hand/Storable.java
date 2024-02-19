bf34b5640be9369b987f626f0c5c8b2f96a2b71e
Roman Kovtun <9696465@gmail.com>
2024-02-20 00:03:22 +0300

    Homework Day6 task8: Completed

--- src/homework/day5/playground/essence/craft/hand/Storable.java
+++ src/homework/day5/playground/essence/craft/hand/Storable.java
@@ -1,4 +1,7 @@
 package homework.day5.playground.essence.craft.hand;
 
+import homework.day5.playground.essence.material.Pourable;
+
 public interface Storable {
+    void store(Pourable pourable, int amount);
 }
--- src/homework/day5/playground/essence/material/Diesel.java
+++ src/homework/day5/playground/essence/material/Diesel.java
@@ -6,4 +6,10 @@ public class Diesel extends Matter implements Pourable, Powerable {
     public Diesel(int mass) {
         super(mass);
     }
+
+    @Override
+    public void spread(String storeName) {
+        System.out.printf("I am %s and I am spreading in %s", getClass().getSimpleName(), storeName).println();
+        System.out.println("whooh..");
+    }
 }
