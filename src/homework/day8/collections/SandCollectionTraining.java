856c5ecff15daeab2ec904c76131291cebcc23a4
romankovtun <9696465@gmail.com>
2024-02-26 23:22:03 +0200

    Homework day8 Task 14: SandCollectionTraining class added

---
+++ src/homework/day8/collections/SandCollectionTraining.java
@@ -0,0 +1,82 @@
+package homework.day8.collections;
+
+import homework.day8.objects.Person;
+import homework.day8.objects.Sand;
+import homework.day8.util.PrinterSymbol;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SandCollectionTraining {
+    public static void main(String[] args) {
+        List<Sand> sandbox = new ArrayList<>();
+        sandbox.add(new Sand(2, "Речной"));
+        sandbox.add(new Sand(4, "Речной"));
+        sandbox.add(new Sand(2, "Карьерный"));
+        sandbox.add(new Sand(7, "Речной"));
+
+        printSandsWeightForEachThrough(sandbox, PrinterSymbol.SPACE);
+        printSandsNamesForEachThrough(sandbox, PrinterSymbol.SPACE);
+
+        Map<Integer, Sand> sandMap = new HashMap<>();
+        for (int i = 0; i < sandbox.size(); i++) {
+            sandMap.put(i, sandbox.get(i));
+        }
+        printSandsMapKeys(sandMap);
+        printSandsMapValues(sandMap);
+        printSandsMapKeyValuePairs(sandMap);
+    }
+
+    public static <T> void printSandsWeightForEachThrough(List<T> inputList, PrinterSymbol printerSymbol) {
+        for (T element : inputList) {
+            if (inputList.indexOf(element) == inputList.size() - 1) {
+                if (element instanceof Sand) {
+                    System.out.print(((Sand) element).getWeight());
+                }
+            } else {
+                if (element instanceof Sand) {
+                    System.out.print(((Sand) element).getWeight() + printerSymbol.getValue());
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static <T> void printSandsNamesForEachThrough(List<T> inputList, PrinterSymbol printerSymbol) {
+        for (T element : inputList) {
+            if (inputList.indexOf(element) == inputList.size() - 1) {
+                if (element instanceof Sand) {
+                    System.out.print(((Sand) element).getName());
+                }
+            } else {
+                if (element instanceof Sand) {
+                    System.out.print(((Sand) element).getName() + printerSymbol.getValue());
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void printSandsMapKeys(Map<Integer, Sand> inputSandsMap) {
+        for (int key : inputSandsMap.keySet()) {
+            System.out.println(key);
+        }
+        System.out.println();
+    }
+
+    public static void printSandsMapValues(Map<Integer, Sand> inputSandsMap) {
+        for (Sand sand : inputSandsMap.values()) {
+            System.out.println(sand);
+        }
+        System.out.println();
+    }
+
+    public static void printSandsMapKeyValuePairs(Map<Integer, Sand> inputSandsMap) {
+        for (int i = 0; i < inputSandsMap.size(); i++) {
+            System.out.println("Key {" + inputSandsMap.keySet().toArray()[i] + "} : Value {" + inputSandsMap.values().toArray()[i] + "}");
+        }
+        System.out.println();
+    }
+}
