ec3290a7fca7792760ac04998045cda8a3f610c5
romankovtun <9696465@gmail.com>
2024-02-28 22:36:23 +0200

    Homework day9: CountriesRunner class created

---
+++ src/homework/day9/CountriesRunner.java
@@ -0,0 +1,12 @@
+package homework.day9;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class CountriesRunner {
+    public static void main(String[] args) {
+        Stream<String> countries = Stream.of("Андора", "Португалия", "Англия", "Замбия");
+        countries.filter(s -> s.matches("[А-ЯЁ][-А-яЁё]+")).filter(s -> s.length() < 7).
+                map(String::toUpperCase).map(s ->"\"" + s + "\"").forEach(System.out::println);
+    }
+}
