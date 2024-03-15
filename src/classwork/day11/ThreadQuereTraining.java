54b592456a70e613eac6e75a071befb547b06769
romankovtun <9696465@gmail.com>
2024-03-15 20:42:33 +0300

    Classwork day11

---
+++ src/classwork/day11/ThreadQuereTraining.java
@@ -0,0 +1,20 @@
+package classwork.day11;
+
+public class ThreadQuereTraining {
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                try {
+                    Thread.currentThread().sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.printf("t1-%s  ", i);
+            }
+        }).start();
+        for (int i = 0; i < 3; i++) {
+            Thread.currentThread().sleep(1000);
+            System.out.printf("m-%s  ", i);
+        }
+    }
+}
