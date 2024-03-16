1a97ef2cbfc8e0054cd85687430d01d1423aff34
romankovtun <9696465@gmail.com>
2024-03-16 12:44:56 +0300

    Homework day11: refactoring

--- src/homework/day11/MouseDeleter1.java
+++ src/homework/day11/MouseDeleter1.java
@@ -1,38 +1,25 @@
 package homework.day11;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MouseDeleter1 {
     public static void main(String[] args) {
-        List<Mouse> mouseList = new ArrayList<>(20);
-        for (int i = 1; i < 281; i++) {
-            mouseList.add(new Mouse(i));
-        }
-        Object lock = new Object();
-        List<Thread> threadList = new ArrayList<>();
-        for (int i = 0; i < 7; i++) {
-            int finalI = i;
-            Thread thread = new Thread(() -> {
-                while (!mouseList.isEmpty()) {
-                    synchronized (lock) {
-                        if (!mouseList.isEmpty()) {
-                            System.out.printf("t%s  removes ", finalI);
-                            Mouse mouse = mouseList.remove(0);
-                            mouse.peep();
-                        }
-                    }
+        List<Mouse> syncMouseList = Collections.synchronizedList(IntStream
+                .range(1, 281)
+                .boxed()
+                .map(Mouse::new)
+                .collect(Collectors.toList()));
+
+        IntStream.range(1, 5).forEach(i -> {
+            new Thread(() -> {
+                while (!syncMouseList.isEmpty()) {
+                    syncMouseList.remove(0).peep();
                 }
-            });
-            thread.start();
-            threadList.add(thread);
-        }
-        for (Thread thread : threadList) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
+            }).start();
+        });
     }
 }
--- src/homework/day11/MouseDeleter2.java
+++ src/homework/day11/MouseDeleter2.java
@@ -1,44 +1,34 @@
 package homework.day11;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MouseDeleter2 {
     public static void main(String[] args) {
-        List<Mouse> mouseList = new ArrayList<>(20);
-        for (int i = 1; i < 381; i++) {
-            mouseList.add(new Mouse(i));
-        }
+        List<Mouse> syncMouseList = Collections.synchronizedList(IntStream
+                .range(1, 381)
+                .boxed()
+                .map(Mouse::new)
+                .collect(Collectors.toList()));
         Object lock = new Object();
-        List<Thread> threadList = new ArrayList<>();
-        for (int i = 0; i < 7; i++) {
-            int finalI = i;
-            Thread thread = new Thread(() -> {
-                while (!mouseList.isEmpty()) {
-                    synchronized (lock) {
-                        if (!mouseList.isEmpty()) {
-                            System.out.printf("t%s  removes ", finalI);
-                            Mouse mouse = mouseList.remove(0);
-                            mouse.peep();
-                            try {
-                                TimeUnit.MILLISECONDS.sleep(250);
-                            } catch (InterruptedException e) {
-                                throw new RuntimeException(e);
-                            }
+        IntStream.range(1, 7).forEach(i -> {
+            synchronized (lock) {
+                new Thread(() -> {
+                    while (!syncMouseList.isEmpty()) {
+                        System.out.printf("Thread %s ", Thread.currentThread().threadId());
+                        syncMouseList.remove(0).peep();
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(250);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
                         }
                     }
-                }
-            });
-            thread.start();
-            threadList.add(thread);
-        }
-        for (Thread thread : threadList) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                }).start();
             }
-        }
+        });
     }
 }
